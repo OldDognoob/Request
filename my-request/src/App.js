94d76f69e390432b813fcbd6444ef82e4fbf716d
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-12-13 19:33:32 +0200

    wip

--- my-request/src/App.js
+++ my-request/src/App.js
@@ -1,23 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 //library
 import axios from "axios";
 
 import "./App.css";
 
-function App(){
+function App() {
+  const [joke, setJoke] = useState("");
+
   const getJoke = () => {
-    axios.get ("https://official-joke-api.appspot.com/random_joke")
-    .then((response)=>{
-      console.log(response);
-    }
-    );
+    fetch("https://official-joke-api.appspot.com/random_joke")
+      .then((response) => response.json())
+      .then((data) => {
+        setJoke(data.setup + "..." + data.punschline);
+      });
   };
-  return <div>Hello Dimos!
-    <button onClick={getJoke}>Get the Joke</button>
-  </div>;
-  
-
-
+  return (
+    <div className="app">
+      <div className="card">
+        <hi1 className = "heading">Hello Dimos!</hi1>
+        <button className="button" onClick={getJoke}>
+          Get the Joke
+        </button>
+        {joke}
+      </div>
+    </div>
+  );
 }
 export default App;
