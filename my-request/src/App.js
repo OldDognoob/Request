b60a9554d8fc76a69a3ea781bac9cd92057a5fd6
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-12-13 18:57:20 +0200

    wip

--- my-request/src/App.js
+++ my-request/src/App.js
@@ -6,6 +6,11 @@ import axios from "axios";
 import "./App.css";
 
 function App(){
+  const getJoke = () => {
+    axios.get ("https://official-joke-api.appspot.com/random_joke");
+  }
+  return <div>Hello Dimos!</div>;
+
 
 }
 export default App;
